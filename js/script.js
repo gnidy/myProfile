b1e123dc15616ef8a66a8d59541cbba33c14b513
Abdo <algnidy1@gmail.com>
2025-05-10 02:31:22 +0300

    update  some change

--- js/script.js
+++ js/script.js
@@ -12,36 +12,3 @@ document.addEventListener("click", function (e) {
 
 
 
-// Form Validation
-const form = document.querySelector('form');
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const name = form.querySelector('input[type="text"]');
-  const email = form.querySelector('input[type="email"]');
-  const message = form.querySelector('textarea');
-
-  // Check if any required fields are empty
-  if (name.value.trim() === '' && email.value.trim() === '' && message.value.trim() === '') {
-    return sweetAlert()
-  } // ok
-
-  // Check if the email is valid               there wrong here about what the wrong
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/i;
-  if (!emailPattern.test(email.value.trim())) {
-    sweetAlertEmail()
-    return; // Stop form submission if email is invalid
-  }else if (name.value.trim() === '' )  {
-    return sweetAlertEmptyName()
-}
-
-  // Simulate form submission (you can add AJAX to actually send the data)
-  if (name.value.trim() !== '' && email.value.trim() !== '') {
-    sweetAlertName(name.value)
-    form.reset(); // Clear the form after submission
-  } else {
-    e.preventDefault()
-  }
-});
-
-
